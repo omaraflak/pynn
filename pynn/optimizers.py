345118a9e7304ccd96144ee541e32570d39e1f4b
Omar Aflak <aflakomar@gmail.com>
2024-11-19 12:20:44 +0000

    Add momentum to SGD

--- pynn/optimizers.py
+++ pynn/optimizers.py
@@ -2,6 +2,7 @@
 
 
 import abc
+import numpy as np
 from pynn import modules
 
 
@@ -18,10 +19,16 @@ class Optimizer(abc.ABC):
 
 
 class SGD(Optimizer):
-    def __init__(self, module: modules.Module, learning_rate: float = 0.1):
+    def __init__(self, module: modules.Module, learning_rate: float = 0.01, momentum: float = 0.9):
         super().__init__(module)
         self.learning_rate = learning_rate
+        self.momentum = momentum
+        self.v = [np.zeros_like(param) for param in module.parameters()]
 
     def step(self):
-        for param, grad in zip(self.module.parameters(), self.module.gradients()):
-            param -= self.learning_rate * grad
+        parameters = self.module.parameters()
+        gradients = self.module.gradients()
+        for i in range(len(parameters)):
+            self.v[i] = self.v[i] * self.momentum + self.learning_rate * gradients[i]
+            parameters[i] -= self.v[i]
+
