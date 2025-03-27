35dca98b4b64e5abb7dd5d74abe16e2a5a683d1b
Omar Aflak <aflakomar@gmail.com>
2025-03-27 18:45:12 +0000

    Fixed iterator

--- ctensor/iterator.cu
+++ ctensor/iterator.cu
@@ -3,36 +3,44 @@
 TensorIterator* iterator_create(Tensor* tensor) {
 	TensorIterator* it = (TensorIterator*) malloc(sizeof(TensorIterator));
 	it->tensor = tensor;
-	it->counters = (int32_t*) malloc(sizeof(int32_t) * tensor->dims);
 	it->idx = tensor->offset;
-	for (int32_t i=0; i<tensor->dims; i++) {
-		it->counters[i] = 0;
+	if (it->tensor->base) {
+		it->counters = (int32_t*) malloc(sizeof(int32_t) * tensor->dims);
+		for (int32_t i=0; i<tensor->dims; i++) {
+			it->counters[i] = 0;
+		}
 	}
 	return it;
 }
 
-bool iterator_has_next(TensorIterator* it) {
-	for (int32_t i=0; i<it->tensor->dims; i++) {
-		if (it->counters[i] != it->tensor->shape[i] - 1) {
-			return false;
+void iterator_next(TensorIterator* it) {
+	if (!it->tensor->base) {
+		it->idx++;
+		if (it->idx == it->tensor->size) {
+			it->idx = -1;
 		}
+		return;
 	}
-	return true;
-}
 
-int32_t iterator_next(TensorIterator* it) {
+	bool increment = false;
 	for (int32_t i=it->tensor->dims - 1; i>=0; i--) {
 		if (it->counters[i] + 1 < it->tensor->shape[i]) {
 			it->counters[i]++;
 			it->idx += it->tensor->stride[i];
+			increment = true;
 			break;
 		}
+		it->idx -= it->counters[i] * it->tensor->stride[i];
 		it->counters[i] = 0;
 	}
-	return it->idx;
+	if (!increment) {
+		it->idx = -1;
+	}
 }
 
 void iterator_free(TensorIterator* it) {
-	free(it->counters);
+	if (it->tensor->base) {
+		free(it->counters);
+	}
 	free(it);
 }
--- ctensor/iterator.h
+++ ctensor/iterator.h
@@ -11,8 +11,7 @@ typedef struct
 } TensorIterator;
 
 TensorIterator* iterator_create(Tensor* tensor);
-bool iterator_has_next(TensorIterator* it);
-int32_t iterator_next(TensorIterator* it);
+void iterator_next(TensorIterator* it);
 void iterator_free(TensorIterator* it);
 
 #endif // ITERATOR
